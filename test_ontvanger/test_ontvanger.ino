78482e5be191c4479eb46d7283b482fe344d4dc4
attinkole <77493658+attinkole@users.noreply.github.com>
2021-01-28 15:17:52 +0100

    Add files via upload

--- test_ontvanger/test_ontvanger.ino
+++ test_ontvanger/test_ontvanger.ino
@@ -2,10 +2,20 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Servo.h>
+#include <math.h>
 
-int LED = 9;
-int LED2 = 6;
+// Define pins
+#define EN_PIN    2  // LOW: Driver disabled. HIGH: Driver enabled (verbind arduino pin 2 met driver pin "SLEEP" en "RESET")
+#define STEP_PIN  3  // Step on rising edge (arduino pin 3 met driver pin "STEP")
+#define DIR_PIN   6  // Motor richting (arduino pin 6 naar driver pin "DIR")
+#define REF_PIN   7  // Referentie schakelaar (arduino pin 7 via drukknop naar ground)
+#define MODE0_PIN 8  // Microstepping selectie (arduino pin 8 naar driver pin "M0")
+#define MODE1_PIN 9  // Microstepping selectie (arduino pin 9 naar driver pin "M1")
+#define MODE2_PIN 10 // Microstepping selectie (arduino pin 10 naar driver pin "M2")
 
+long ustep = 8;      // Microstepping = 1, 2, 4, 8, 16, 32
+long rpm = 30;      // Toerental in rpm
+long omw = 400;      // Aantal volle stappen per omwenteling (360 / staphoek in graden)
 
 RF24 radio(7, 8);   // nRF24L01 (CE, CSN)
 const byte address[6] = "00001";
@@ -19,6 +29,9 @@ Servo servo2;
 int servoValue1, servoValue2;
 int pan = 90;
 int tilt = 90;
+int positie = omw * ustep;
+int homing = 0;
+int steptime = 60000000 / (omw * ustep * rpm);
 
 struct Data_Package {
   byte joy1x;
@@ -42,8 +55,22 @@ void setup() {
   
   servo1.attach(4);
   servo2.attach(5);
-  pinMode(LED, OUTPUT);
-  pinMode(LED2, OUTPUT);
+  
+  // Prepare pins
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(REF_PIN, INPUT_PULLUP);
+  pinMode(MODE0_PIN, OUTPUT);
+  pinMode(MODE1_PIN, OUTPUT);
+  pinMode(MODE2_PIN, OUTPUT);
+
+  double mode = log(ustep) / log(2);
+  int intmode = (int) mode;
+  digitalWrite(MODE0_PIN, intmode & B00000001);
+  digitalWrite(MODE1_PIN, intmode & B00000010);
+  digitalWrite(MODE2_PIN, intmode & B00000100);
+  digitalWrite(EN_PIN, HIGH);    // Enable driver in hardware
 }
 
 void loop() {
@@ -56,16 +83,32 @@ void loop() {
     lastReceiveTime = millis();
   }
 
-  if(data.button1 == 0) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
+  // begin stepper code
+
+  if(homing == 0 && digitalRead(REF_PIN) == LOW){   // Linksom draaien tot eindstop hoog wordt
+    positie = 0;
+    homing = 1;
   }
-  if(data.button2 == 0) {
-    digitalWrite(LED2, HIGH);
-  } else {
-    digitalWrite(LED2, LOW);
+
+  else if(homing == 1 && positie == omw * ustep / 2){   // Naar midden van slag gaan(halfe rotatie)
+    homing = 2;
   }
+  
+  else if(data.button1 == 0 && positie != 0 || homing == 0 && positie != 0){   // Linksom draaien
+    digitalWrite(DIR_PIN, HIGH);
+    digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+    delayMicroseconds(steptime);
+    positie--;
+  }
+
+  else if(data.button2 == 0 && positie != omw * ustep || homing == 1){   // Rechtsom daaien
+    digitalWrite(DIR_PIN, LOW);
+    digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+    delayMicroseconds(steptime);
+    positie++;
+  }
+
+  // eind stepper code
 
   servoValue1 = map(data.joy1x, 0, 255, 0, 180);
     if(servoValue1 > 95 && pan < 181)
